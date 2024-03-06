a4febdd771548af929f76c67101b5204a261000b
SeInLee26 <108651556+SeInLee26@users.noreply.github.com>
2024-03-06 20:53:25 +0900

    [Bronze V] Title: 윤년, Time: 132 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,6 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+let year = Number(input[0]);
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)
+console.log("1");
+else
+console.log("0");
