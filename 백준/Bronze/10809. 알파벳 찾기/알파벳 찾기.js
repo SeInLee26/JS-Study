97c0f5fbfe5934e6af251500d843575d69c760ae
SeInLee26 <108651556+SeInLee26@users.noreply.github.com>
2024-03-15 17:35:49 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const file = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(file).toString();
+const result = [];
+for(let i=97; i<=122; i++)
+{
+    result.push(input.indexOf(String.fromCharCode(i)));
+}
+console.log(result.join(" "));
