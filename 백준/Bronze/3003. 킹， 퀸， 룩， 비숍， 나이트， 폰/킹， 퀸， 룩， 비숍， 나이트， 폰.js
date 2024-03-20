ada22c7a083857a9e4fb387a2ae7b215895f1460
SeInLee26 <108651556+SeInLee26@users.noreply.github.com>
2024-03-20 23:53:56 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 128 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.js
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const file = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(file).toString().split(" ");
 const arr = [1, 1, 2, 2, 2, 8];
-let newArr = [];
+let result = ''
 for(let i=0; i<input.length; i++)
 {
-    newArr.push(Number(arr[i]) - Number(input[i]));
+    result += (arr[i]-input[i]) + ' ';
 }
-console.log(newArr.join(" "));
+console.log(result.trim());
