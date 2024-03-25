8b3f89114adaf829ef396fddd186b43cc3d7d783
SeInLee26 <108651556+SeInLee26@users.noreply.github.com>
2024-03-26 01:00:18 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    var answer = '';
+    if(s.length % 2 === 0)
+        return `${s[Math.floor(s.length/2)-1]}${s[Math.floor(s.length/2)]}`
+    else
+        return s[Math.floor(s.length/2)];
+    return answer;
+}
