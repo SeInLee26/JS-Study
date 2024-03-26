fa6c7155f1664059524d5ef41760d6aa32083622
SeInLee26 <108651556+SeInLee26@users.noreply.github.com>
2024-03-26 14:18:40 +0900

    [level 1] Title: 직사각형 별찍기, Time: 43.64 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -1,13 +1,11 @@
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
-    const a = Number(n[0]), b = Number(n[1]);
-    let arr = "";
-    for(let i=0; i<b; i++)
-    {
-        for(let j=0; j<a; j++)
-            arr  += '*';
-        arr += '\n'
+    const a = Number(n[0]), b = Number(n[1]); 
+    
+    const str = "*".repeat(a); 
+    for(let i = 0; i < b; i++) {
+        console.log(str); 
     }
-    console.log(arr);
+    
 });
