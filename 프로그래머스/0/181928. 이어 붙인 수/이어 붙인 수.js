d531b66ee11f83f83db08adb5ac55aa39d93fd34
SeInLee26 <108651556+SeInLee26@users.noreply.github.com>
2024-03-31 20:50:59 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,7 @@
+function solution(num_list) {
+    let isEven = num_list.filter((a) => a % 2 === 0);
+    let isOdd = num_list.filter((a) => a % 2 === 1);
+    let evenSum = isEven.reduce((acc, cur) => acc + String(cur));
+    let oddSum = isOdd.reduce((acc, cur) => acc + String(cur));
+    return Number(evenSum) + Number(oddSum);
+}
