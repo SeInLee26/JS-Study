5d32f3264cc7a4194829edaeef3c2ef24c393058
SeInLee26 <108651556+SeInLee26@users.noreply.github.com>
2024-04-13 00:32:54 +0900

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -0,0 +1,8 @@
+function solution(order) {
+    var answer = 0;
+    order = String(order).split('');
+    for(let i=0; i<order.length; i++)
+        if(order[i] === '3' || order[i] === '6' || order[i] === '9')
+            answer++;
+    return answer;
+}
